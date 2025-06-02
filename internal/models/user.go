841d78b5aca0a0bd295cd2308725a35302156b99
migvas <migas.v94@gmail.com>
2025-06-02 18:26:48 +0100

    fix: Fix unique tag

--- internal/models/user.go
+++ internal/models/user.go
@@ -7,7 +7,7 @@ import (
 type User struct {
 	gorm.Model
 	Name     string
-	Email    string `gorm:"uniqueIndex;not null"`
+	Email    string `gorm:"unique;not null"`
 	Password string `gorm:"not null"`
 	Active   bool
 }
